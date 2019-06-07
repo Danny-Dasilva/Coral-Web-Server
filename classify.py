c1bcd55a996811cdd804021fc90f51d438160ba6
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-07 17:41:51 -0400

    removed requirements

--- classify.py
+++ classify.py
@@ -154,10 +154,6 @@ def render_gen(args):
             engine = next(engines)
 
 def add_render_gen_args(parser):
-    parser.add_argument('--model', required=True,
-                        help='.tflite model path')
-    parser.add_argument('--labels', required=True,
-                        help='label file path')
     parser.add_argument('--window', type=int, default=10,
                         help='number of frames to accumulate inference results')
     parser.add_argument('--top_k', type=int, default=1,
